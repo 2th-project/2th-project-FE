9f4513be3b123f2decdf9bafd0f80a71e02704d8
eunjoodev <eunjoo.dev@gmail.com>
2024-09-05 16:04:34 +0900

    헤더-소통광장 페이지 이동 로직

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,20 @@
 import React from "react";
 import styles from "./Header.module.css";
 import { Outlet } from "react-router-dom";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Header = () => {
   const location = useLocation();
+  const navigate = useNavigate();
 
   const isMainPage = location.pathname === "/";
 
+  const handleNavigate = (path) => {
+    navigate(path);
+  };
+
   return (
     <header className={styles.header} data-page={isMainPage ? "main" : "other"}>
-      {/* 유틸리티 바 */}
       <div className={styles.utilityBar}>
         <div>
           <span className={styles.utilityItem}>공인인증서 내보내기</span>
@@ -20,10 +24,14 @@ const Header = () => {
         </div>
       </div>
 
-      {/* 메인 헤더 */}
       <div className={styles.mainHeader}>
         <div className={styles.logo}>
-          <img src="/assets/logo.png" alt="로고" className={styles.logoImage} />
+          <img
+            src="/assets/logo.png"
+            alt="로고"
+            className={styles.logoImage}
+            onClick={() => handleNavigate("/")}
+          />
         </div>
         <div className={styles.userMenu}>
           <span className={styles.userMenuItem}>통합검색</span>
@@ -34,10 +42,16 @@ const Header = () => {
 
       <hr className={styles.divider} />
 
-      {/* 네비게이션 메뉴 */}
       <nav className={styles.navbar}>
-        <span className={styles.navItem}>복지서비스</span>
-        <span className={styles.navItem}>소통광장</span>
+        <span className={styles.navItem} onClick={() => handleNavigate("/")}>
+          복지서비스
+        </span>
+        <span
+          className={styles.navItem}
+          onClick={() => handleNavigate("/community")}
+        >
+          소통광장
+        </span>
       </nav>
 
       <hr className={styles.divider02} />
