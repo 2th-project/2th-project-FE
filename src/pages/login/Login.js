23ee2aefb4c183a2ab9f4c05a0a2120e526d5e23
KORKUNS <168164132+KORKUNS@users.noreply.github.com>
2024-09-05 17:56:08 +0900

    feat:비밀번호 찾기 모달 추가

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Login.module.css";
 
 const Login = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  const passwordModal = (
+    <div className={styles.modalOverlay}>
+      <div className={styles.modalContent}>
+        <div>비밀번호 찾기</div>
+        <div className={styles.modalInput}>
+          <input
+            type="text"
+            id="findId"
+            name="findId"
+            placeholder="아이디를 입력하세요"
+          />
+        </div>
+        <div className={styles.modalInput}>
+          <input
+            type="text"
+            id="findId"
+            name="findId"
+            placeholder="새 비밀번호"
+          />
+        </div>
+        <div className={styles.modalInput}>
+          <input
+            type="text"
+            id="findId"
+            name="findId"
+            placeholder="비밀번호 확인"
+          />
+        </div>
+        <div className={styles.modalButtons}>
+          <button className={styles.submitButton}>제출</button>
+          <button className={styles.closeButton} onClick={closeModal}>
+            닫기
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+
   return (
     <div className={styles.styleBox}>
       <div className={styles.loginBox}>
@@ -19,10 +66,13 @@ const Login = () => {
             />
           </div>
           <button className={styles.loninButton}>로그인</button>
-          <button className={styles.findPassword}>비밀번호 찾기</button>
+          <button className={styles.findPassword} onClick={openModal}>
+            비밀번호 찾기
+          </button>
         </div>
         <button className={styles.oauthLogin}>소셜 로그인</button>
       </div>
+      {isOpen && passwordModal}
     </div>
   );
 };
--- src/pages/sign/Sign.js
+++ src/pages/sign/Sign.js
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Sign.module.css";
 
 const Sign = () => {
+  const [userId, setUserId] = useState("");
+  const [userName, setUserName] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+  const [userPasswordCheck, setUserPasswordCheck] = useState("");
+  const [userEmail, setUserEmail] = useState("");
+  const [userBirthday, setUserBirthday] = useState("");
+  const [userPhoneNumber, setUserPhoneNumber] = useState("");
+  const [userArea, setUserArea] = useState("");
+
+  const changeIdHandler = (event) => {
+    setUserId(event.target.value);
+  };
+
+  const changeNameHandler = (event) => {
+    setUserName(event.target.value);
+  };
+
+  const changePasswordHandler = (event) => {
+    setUserPassword(event.target.value);
+  };
+
+  const changePasswordCheckHandler = (event) => {
+    setUserPasswordCheck(event.target.value);
+  };
+
+  const changeEmailHandler = (event) => {
+    setUserEmail(event.target.value);
+  };
+
+  const changeBirthdayHandler = (event) => {
+    setUserBirthday(event.target.value);
+  };
+
+  const changePhoneNumberHandler = (event) => {
+    setUserPhoneNumber(event.target.value);
+  };
+
+  const changeAreaHandler = (event) => {
+    setUserArea(event.target.value);
+  };
+
   return (
     <div className={styles.pagebox}>
       <div className={styles.signbox}>
@@ -9,46 +50,81 @@ const Sign = () => {
         <form className={styles.form}>
           <div className={styles.formGroup}>
             <label htmlFor="id">아이디</label>
-            <input type="text" id="id" name="id" />
+            <input type="text" id="id" name="id" onChange={changeIdHandler} />
             <div>
               <button className={styles.checkButton}>중복확인</button>
             </div>
           </div>
           <div className={styles.formGroup}>
             <label htmlFor="name">이름</label>
-            <input type="text" id="name" name="name" />
+            <input
+              type="text"
+              id="name"
+              name="name"
+              onChange={changeNameHandler}
+            />
             <div></div>
           </div>
           <div className={styles.formGroup}>
             <label htmlFor="birthday">생년월일</label>
-            <input type="date" id="birthday" name="birthday" />
+            <input
+              type="date"
+              id="birthday"
+              name="birthday"
+              onChange={changeBirthdayHandler}
+            />
             <div></div>
           </div>
           <div className={styles.formGroup}>
             <label htmlFor="email">이메일</label>
-            <input type="email" id="email" name="email" />
+            <input
+              type="email"
+              id="email"
+              name="email"
+              onChange={changeEmailHandler}
+            />
             <div>
               <button className={styles.checkButton}>중복확인</button>
             </div>
           </div>
           <div className={styles.formGroup}>
             <label htmlFor="password">비밀번호</label>
-            <input type="password" id="password" name="password" />
+            <input
+              type="password"
+              id="password"
+              name="password"
+              onChange={changePasswordHandler}
+            />
             <div></div>
           </div>
           <div className={styles.formGroup}>
             <label htmlFor="password_check">비밀번호 확인</label>
-            <input type="password" id="password_check" name="password_check" />
+            <input
+              type="password"
+              id="password_check"
+              name="password_check"
+              onChange={changePasswordCheckHandler}
+            />
             <div></div>
           </div>
           <div className={styles.formGroup}>
             <label htmlFor="phone_number">연락처</label>
-            <input type="number" id="phone_number" name="phone_number" />
+            <input
+              type="number"
+              id="phone_number"
+              name="phone_number"
+              onChange={changePhoneNumberHandler}
+            />
             <div></div>
           </div>
           <div className={styles.formGroup}>
             <label htmlFor="area">거주 지역</label>
-            <input type="text" id="area" name="area" />
+            <input
+              type="text"
+              id="area"
+              name="area"
+              onChange={changeAreaHandler}
+            />
             <div></div>
           </div>
           <div className={styles.submit}>
