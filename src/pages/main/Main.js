13cefa53b6a6eda47aecab60dd348a7aea9d8267
eunjoodev <eunjoo.dev@gmail.com>
2024-09-07 01:07:42 +0900

    메인페이지 필터링 컴포넌트 수정

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -12,7 +12,7 @@ const Main = () => {
         <MainBanner />
         <KeyBenefits />
         <QuickFind />
-        <FilterComponent />
+        {/* <FilterComponent /> */}
         <WelfareList />
       </main>
     </>
